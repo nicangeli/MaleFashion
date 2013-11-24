1d96566ef605b4b28f4c3fa6fd9af62b9d1a3655
Nicholas Angeli <nicangeli@gmail.com>
2013-11-24 23:22:55 +0000

    added segue to categoriesviewontroller

---
+++ MaleHighstreet/CategoriesViewController.h
@@ -0,0 +1,13 @@
+//
+//  CategoriesViewController.h
+//  MaleHighstreet
+//
+//  Created by Nicholas Angeli on 24/11/2013.
+//  Copyright (c) 2013 NicholasAngeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoriesViewController : UIViewController
+
+@end
---
+++ MaleHighstreet/CategoriesViewController.m
@@ -0,0 +1,38 @@
+//
+//  CategoriesViewController.m
+//  MaleHighstreet
+//
+//  Created by Nicholas Angeli on 24/11/2013.
+//  Copyright (c) 2013 NicholasAngeli. All rights reserved.
+//
+
+#import "CategoriesViewController.h"
+
+@interface CategoriesViewController ()
+
+@end
+
+@implementation CategoriesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
