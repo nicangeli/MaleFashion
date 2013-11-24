4dbb94f012a2bcae420ab3a8325ce7c381c22518
Nicholas Angeli <nicangeli@gmail.com>
2013-11-24 22:23:15 +0000

    onboarding images and scrollview in place

--- MaleHighstreet/OnboardingViewController.m
+++ MaleHighstreet/OnboardingViewController.m
@@ -34,6 +34,115 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.pageImages = [[NSMutableArray alloc] init];
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self setUp];
+}
+
+-(void)setUp
+{
+    [self.pageImages addObject:[UIImage imageNamed:@"sporty.png"]];
+    [self.pageImages addObject:[UIImage imageNamed:@"casual.png"]];
+    [self.pageImages addObject:[UIImage imageNamed:@"smartCasual.png"]];
+    [self.pageImages addObject:[UIImage imageNamed:@"office.png"]];
+    [self.pageImages addObject:[UIImage imageNamed:@"classic.png"]];
+    
+    
+    NSInteger pageCount = [self.pageImages count];
+    
+    // 2
+    self.pageControl.currentPage = 0;
+    self.pageControl.numberOfPages = pageCount;
+    
+    // 3
+    self.pageViews = [[NSMutableArray alloc] init];
+    for (NSInteger i = 0; i < pageCount; ++i) {
+        [self.pageViews addObject:[NSNull null]];
+    }
+    
+    
+    
+    // 4
+    CGSize pagesScrollViewSize = self.scrollView.frame.size;
+    self.scrollView.contentSize = CGSizeMake(pagesScrollViewSize.width * self.pageImages.count, pagesScrollViewSize.height);
+    
+    // 5
+    //[self.view setNeedsDisplay];
+    [self loadVisiblePages];
+}
+
+-(void)loadPage:(NSInteger)page
+{
+    if(page < 0 || page >= self.pageImages.count) {
+        return;
+    }
+    
+    UIView *pageView = [self.pageViews objectAtIndex:page];
+    if((NSNull *)pageView == [NSNull null]) {
+        CGRect frame = self.scrollView.bounds;
+        frame.origin.x = frame.size.width * page;
+        frame.origin.y = 0.0f;
+        
+        UIImageView *newPageView = [[UIImageView alloc] initWithImage:[self.pageImages objectAtIndex:page]];
+        newPageView.contentMode = UIViewContentModeScaleAspectFit;
+        newPageView.frame = frame;
+        [self.scrollView addSubview:newPageView];
+        [newPageView reloadInputViews];
+        
+        [self.pageViews replaceObjectAtIndex:page withObject:newPageView];
+        
+    }
+}
+
+- (void)purgePage:(NSInteger)page {
+    if (page < 0 || page >= self.pageImages.count) {
+        // If it's outside the range of what you have to display, then do nothing
+        return;
+    }
+    
+    // Remove a page from the scroll view and reset the container array
+    UIView *pageView = [self.pageViews objectAtIndex:page];
+    if ((NSNull*)pageView != [NSNull null]) {
+        [pageView removeFromSuperview];
+        [self.pageViews replaceObjectAtIndex:page withObject:[NSNull null]];
+    }
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    // Load the pages that are now on screen
+    [self loadVisiblePages];
+}
+
+- (void)loadVisiblePages {
+    // First, determine which page is currently visible
+    CGFloat pageWidth = self.scrollView.frame.size.width;
+    NSInteger page = (NSInteger)floor((self.scrollView.contentOffset.x * 2.0f + pageWidth) / (pageWidth * 2.0f));
+    
+    // Update the page control
+    self.pageControl.currentPage = page;
+    
+    // Work out which pages you want to load
+    NSInteger firstPage = page - 1;
+    NSInteger lastPage = page + 1;
+    
+    // Purge anything before the first page
+    for (NSInteger i=0; i<firstPage; i++) {
+        [self purgePage:i];
+    }
+    
+	// Load pages in our range
+    for (NSInteger i=firstPage; i<=lastPage; i++) {
+        [self loadPage:i];
+    }
+    
+	// Purge anything after the last page
+    for (NSInteger i=lastPage+1; i<self.pageImages.count; i++) {
+        [self purgePage:i];
+    }
 }
 
 - (void)didReceiveMemoryWarning
